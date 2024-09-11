8c287f5f3ec6fa8e490a06afa958abd5477bf064
Emeline Farget <emeline.farget@gmail.com>
2024-09-11 21:01:41 +0200

    Correction d'un problème d'actualisation lors du déploiement

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes} from "react-router-dom";
+import { Route, Routes, HashRouter } from "react-router-dom";
 import withRouter from "../hooks/withRouter"
 import { Home } from "../pages/home";
 import { Portfolio } from "../pages/portfolio";
@@ -32,10 +32,12 @@ const AnimatedRoutes = withRouter(({ location }) => (
 
 function AppRoutes() {
   return (
-    <div className="s_c">
-      <AnimatedRoutes />
-      <Socialicons />
-    </div>
+    <HashRouter>
+      <div className="s_c">
+        <AnimatedRoutes />
+        <Socialicons />
+      </div>
+    </HashRouter>
   );
 }
 
